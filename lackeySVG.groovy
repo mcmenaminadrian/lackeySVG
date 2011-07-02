748ab497c3a7fcda0f6b7f032ce3be9d06859d07
Adrian McMenamin <adrianmcmenamin@gmail.com>
2011-07-02 11:36:53 +0100

    being clear on lifetime function

--- lackeySVG.groovy
+++ lackeySVG.groovy
@@ -74,7 +74,7 @@ class LackeySVGraph {
 				def saxReader = SAXParserFactory.newInstance().newSAXParser().XMLReader
 				saxReader.setContentHandler(handler4)
 				saxReader.parse(new InputSource(new FileInputStream(fPath)))
-				thetaMap[steps]=steps/handler4.faults
+				thetaMap[steps] = handler.totalInstructions / handler4.faults
 			}
 			pool.submit(pass as Callable)
 		}
