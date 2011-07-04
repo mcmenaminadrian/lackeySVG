d7f07037dc406a567563ff28dcf05f1a86223b80
Adrian McMenamin <adrianmcmenamin@gmail.com>
2011-07-04 23:56:49 +0100

    better answer to performance problem??

--- lackeySVG.groovy
+++ lackeySVG.groovy
@@ -91,13 +91,10 @@ class LackeySVGraph {
 					def g = (int)(handler.totalInstructions /
 						handler4.faults)
 					thetaMap[steps] = g
-					println "step 0"
 					BigInteger sumMem = handler4.sizes.sum(0G)
-					println "step 1"
-					def avgWSS = sumMem/handler4.sizes.size()
-					println "step 2"
+					def avgWSS = sumMem/handler.totalInstructions
 					thetaAveMap[avgWSS] = g
-					println "step 3" 
+					println "Ave. working set $avgWSS"
 
 				}
 				pool.submit(passWS as Callable)
